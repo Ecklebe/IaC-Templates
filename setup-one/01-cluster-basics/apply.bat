4b90ea039d93efb8fdb204dd7cc76b653a99b1d2
Tobias Ecklebe <tobias.ecklebe@outlook.de>
2022-04-02 11:59:18 +0200

    add a script to call the operation steps

--- setup-one/01-cluster-basics/apply.bat
+++ setup-one/01-cluster-basics/apply.bat
@@ -1,4 +1,6 @@
-(terraform -chdir=.\01-extending-kubernetes\ init) ^
-&& (terraform -chdir=.\01-extending-kubernetes\ apply -auto-approve) ^
-&& (terraform -chdir=.\02-setup\ init) ^
-&& (terraform -chdir=.\02-setup\ apply -auto-approve)
+(terraform -chdir=.\01-extending-kubernetes\ init -input=false) ^
+&& (terraform -chdir=.\01-extending-kubernetes\ plan -var-file=..\..\terraform.tfvars -out=output.tfplan -input=false -compact-warnings) ^
+&& (terraform -chdir=.\01-extending-kubernetes\ apply -auto-approve -input=false -compact-warnings "output.tfplan") ^
+&& (terraform -chdir=.\02-setup\ init -input=false) ^
+&& (terraform -chdir=.\02-setup\ plan -var-file=..\..\terraform.tfvars -out=output.tfplan -input=false -compact-warnings) ^
+&& (terraform -chdir=.\02-setup\ apply -auto-approve -input=false -compact-warnings "output.tfplan")
