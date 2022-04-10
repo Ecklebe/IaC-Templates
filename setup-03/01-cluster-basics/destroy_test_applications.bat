91a39914f45f34797c2835a2d25e7e1d45072fc8
Tobias Ecklebe <tobias.ecklebe@outlook.de>
2022-04-10 11:56:54 +0200

    add destroy script

---
+++ setup-03/01-cluster-basics/destroy_test_applications.bat
@@ -0,0 +1,2 @@
+(terraform -chdir=.\04-test-applications\ plan -destroy -var-file=..\..\terraform.tfvars -out=main.destroy.tfplan -input=false -compact-warnings) ^
+&& (terraform -chdir=.\04-test-applications\ apply -auto-approve -input=false -compact-warnings "main.destroy.tfplan")
