1317c1c56028990eb970401f191622c8566d8ef6
Tobias Ecklebe <tobias.ecklebe@outlook.de>
2022-04-10 13:23:31 +0200

    activate https for the grafana dashboard

--- setup-03/01-cluster-basics/03-setup/03-grafana-dashboard.tf
+++ setup-03/01-cluster-basics/03-setup/03-grafana-dashboard.tf
@@ -7,19 +7,19 @@ resource "kubernetes_manifest" "grafana-dashboard-ingress-route" {
   manifest = {
     "apiVersion" = "traefik.containo.us/v1alpha1"
     "kind"       = "IngressRoute"
-    "metadata"   = {
+    "metadata" = {
       "name"      = "grafana-dashboard"
       "namespace" = var.monitoring_namespace
     }
-    "spec"       = {
-      "entryPoints" = ["web"]
-      #"tls" = {
-      #  "secretName" = kubernetes_secret.signed-tls-2.metadata[0].name
-      #}
-      "routes"      = [
+    "spec" = {
+      "entryPoints" = ["websecure"]
+      "tls" = {
+        "secretName" = kubernetes_secret.signed-tls-2.metadata[0].name
+      }
+      "routes" = [
         {
-          "match"    = "Host(`grafana.localhost`)"
-          "kind"     = "Rule"
+          "match" = "Host(`grafana.localhost`)"
+          "kind"  = "Rule"
           "services" = [
             {
               "name" = "prometheus-grafana"
