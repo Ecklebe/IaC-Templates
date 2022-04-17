3d5081883072009c7c6cdc7a0d6d6927bc36ca34
Tobias Ecklebe <tobias.ecklebe@outlook.de>
2022-04-17 23:38:54 +0200

    split the needed parts for the traefik metrics endpoint to a separated terraform step

--- setup-03/01-cluster-basics/06-install-traefik-metrics/03-traefik-metrics.tf
+++ setup-03/01-cluster-basics/06-install-traefik-metrics/03-traefik-metrics.tf
@@ -3,100 +3,13 @@ https://traefik.io/blog/capture-traefik-metrics-for-apps-on-kubernetes-with-prom
 https://community.traefik.io/t/capture-traefik-metrics-for-apps-on-kubernetes-with-prometheus/9811
 */
 
+variable "monitoring_namespace" {
+  type = string
+}
+
 variable "traefik_namespace" {
   description = "Name of the namespace where Traefik will be located"
   type        = string
-}
-
-variable "traefik_admin_username" {
-  description = "Name of the admin account"
-  type        = string
-}
-
-variable "traefik_admin_password" {
-  description = "Password of the admin account"
-  type        = string
-}
-
-resource "kubernetes_secret" "traefik-dashboard-auth" {
-  metadata {
-    name      = "auth"
-    namespace = var.traefik_namespace
-  }
-  data = {
-    username = var.traefik_admin_username
-    password = var.traefik_admin_password
-  }
-
-  type = "kubernetes.io/basic-auth"
-}
-
-resource "kubernetes_manifest" "traefik-dashboard-middleware" {
-  manifest = {
-    "apiVersion" = "traefik.containo.us/v1alpha1"
-    "kind"       = "Middleware"
-    "metadata"   = {
-      "name"      = "dashboard-middleware"
-      "namespace" = var.traefik_namespace
-    }
-    "spec"       = {
-      "basicAuth" = {
-        "secret" = kubernetes_secret.traefik-dashboard-auth.metadata[0].name
-      }
-    }
-  }
-}
-
-resource "kubernetes_manifest" "traefik-redirect-middleware" {
-  manifest = {
-    "apiVersion" = "traefik.containo.us/v1alpha1"
-    "kind"       = "Middleware"
-    "metadata"   = {
-      "name"      = "redirect-middleware"
-      "namespace" = var.traefik_namespace
-    }
-    "spec"       = {
-      "redirectScheme" = {
-        "scheme"    = "https"
-        "permanent" = "true"
-      }
-    }
-  }
-}
-
-resource "kubernetes_manifest" "traefik-dashboard-ingress-route" {
-  manifest = {
-    "apiVersion" = "traefik.containo.us/v1alpha1"
-    "kind"       = "IngressRoute"
-    "metadata"   = {
-      "name"      = "traefik-dashboard2"
-      "namespace" = var.traefik_namespace
-    }
-    "spec"       = {
-      "entryPoints" = ["websecure"]
-      "tls"         = {
-        "secretName" = kubernetes_secret.signed-tls-2.metadata[0].name
-      }
-      "routes"      = [
-        {
-          "match"       = "Host(`traefik.localhost`)"
-          "kind"        = "Rule"
-          "middlewares" = [
-            {
-              "name"      = kubernetes_manifest.traefik-dashboard-middleware.manifest.metadata.name
-              "namespace" = var.traefik_namespace
-            }
-          ]
-          "services"    = [
-            {
-              "name" = "api@internal"
-              "kind" = "TraefikService"
-            }
-          ]
-        }
-      ]
-    }
-  }
 }
 
 resource "kubernetes_manifest" "traefik-metrics-service-monitor" {
