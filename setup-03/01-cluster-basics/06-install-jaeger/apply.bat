1bfb537d1d4be7646cb09485e05bacc1d5f35152
Tobias Ecklebe <tobias.ecklebe@outlook.de>
2022-04-23 12:50:22 +0200

    set default dev-env to linux

--- setup-03/01-cluster-basics/06-install-jaeger/apply.bat
+++
@@ -1,3 +0,0 @@
-(terraform init -input=false) ^
-&& (terraform plan -var-file=..\..\terraform.tfvars -out=output.tfplan -input=false -compact-warnings) ^
-&& (terraform apply -auto-approve -input=false -compact-warnings "output.tfplan")
