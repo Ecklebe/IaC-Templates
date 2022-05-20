4dc57a00ce9cea86bb4a31f1883e9ef23e247408
Tobias Ecklebe <tobias.ecklebe@outlook.de>
2022-05-20 13:45:35 +0200

    Fix configuration so that container can start, test to reach webservice from outside the virtualbox is next..

--- setup-03/00-create-multi-node-kubernetes-cluster/scripts/install-cni.sh
+++ setup-03/00-create-multi-node-kubernetes-cluster/scripts/install-cni.sh
@@ -2,45 +2,48 @@
 
 set -eu -o pipefail
 
+CNI_VERSION=$1
 CNI_CONFIG_DIR=/etc/cni/net.d
+ARCHITECTURE=$2
+OS_TYPE=$3
 
-wget -P /tmp/downloads/cni https://github.com/containernetworking/plugins/releases/download/v1.1.1/cni-plugins-linux-amd64-v1.1.1.tgz
+wget -P /tmp/downloads/cni https://github.com/containernetworking/plugins/releases/download/v"$CNI_VERSION"/cni-plugins-"$OS_TYPE"-"$ARCHITECTURE"-v"$CNI_VERSION".tgz
 mkdir -p /opt/cni/bin
-tar Cxzvf /opt/cni/bin /tmp/downloads/cni/cni-plugins-linux-amd64-v1.1.1.tgz
+tar Cxzvf /opt/cni/bin /tmp/downloads/cni/cni-plugins-"$OS_TYPE"-"$ARCHITECTURE"-v"$CNI_VERSION".tgz
 rm -rf /tmp/downloads/cni
 
 mkdir -p $CNI_CONFIG_DIR
 cat << EOF | tee $CNI_CONFIG_DIR/10-containerd-net.conflist
 {
-  "cniVersion": "1.1.1",
-  "name": "containerd-net",
-  "plugins": [
-    {
-      "type": "bridge",
-      "bridge": "cni0",
-      "isGateway": true,
-      "ipMasq": true,
-      "promiscMode": true,
-      "ipam": {
-        "type": "host-local",
-        "ranges": [
-          [{
-            "subnet": "10.88.0.0/16"
-          }],
-          [{
-            "subnet": "2001:4860:4860::/64"
-          }]
-        ],
-        "routes": [
-          { "dst": "0.0.0.0/0" },
-          { "dst": "::/0" }
-        ]
-      }
-    },
-    {
-      "type": "portmap",
-      "capabilities": {"portMappings": true}
-    }
-  ]
+ "cniVersion": "0.4.0",
+ "name": "containerd-net",
+ "plugins": [
+   {
+     "type": "bridge",
+     "bridge": "cni0",
+     "isGateway": true,
+     "ipMasq": true,
+     "promiscMode": true,
+     "ipam": {
+       "type": "host-local",
+       "ranges": [
+         [{
+           "subnet": "10.88.0.0/16"
+         }],
+         [{
+           "subnet": "2001:db8:4860::/64"
+         }]
+       ],
+       "routes": [
+         { "dst": "0.0.0.0/0" },
+         { "dst": "::/0" }
+       ]
+     }
+   },
+   {
+     "type": "portmap",
+     "capabilities": {"portMappings": true}
+   }
+ ]
 }
 EOF
